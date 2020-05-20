d6482d0f932ce7768ae836586a6a51e59b674bb4
David Sheinbein <davidsheinbein@Davids-MacBook-Pro.local>
2020-05-20 16:00:08 -0700

    update complete

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,10 +1,24 @@
 const skillDB = require('../models/skill');
 
+module.exports = {
+    index,
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
+}
+
 function index(req, res, next) {
     res.render('skills/index', {
         skills: skillDB.getAll()
+        // skills: [
+        //     {skill: 'takewondo', level: 3, id: 100},
+        //     {skill: 'stuff', level: 5, id: 12232},
+        // ]
     })
- }
+}
 
 function show(req, res, next){
     console.log(req.params, "<-- req.params")
@@ -13,7 +27,28 @@ function show(req, res, next){
     })
 }
 
-  module.exports = {
-      index,
-      show
+function newSkill(req, res) {
+    res.render('skills/new.ejs');
+}
+
+function create(req, res) {
+    console.log(req.body);
+    skillDB.create(req.body);
+    res.redirect('/skills');
+}
+
+function deleteSkill(req, res) {
+    skillDB.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function edit(req, res) {
+    const skill = skillDB.getOne(req.params.id);
+    res.render('skills/edit', {skill});
+}
+
+function update(req, res) {
+    req.body.done = !!req.body.done;
+    skillDB.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
   }
--- models/skill.js
+++ models/skill.js
@@ -1,14 +1,17 @@
 const skills = [
-    {id: 100001, skill: 'Karate', done: false},
-    {id: 100002, skill: 'Massage', done: true},
-    {id: 100003, skill: 'Shopping', done: false},
-    {id: 100004, skill: 'Cooking', done: true},
-    {id: 100005, skill: 'Sleeping', done: true}
+    {id: 100001, skill: 'Karate', done: false, level: 'Hard'},
+    {id: 100002, skill: 'Massage', done: true, level: 'Moderate'},
+    {id: 100003, skill: 'Shopping', done: false, level: 'Easy'},
+    {id: 100004, skill: 'Cooking', done: true, level: 'Easy'},
+    {id: 100005, skill: 'Sleeping', done: true, level: 'Hard'}
   ];
 
   module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
   };
 
   function getAll() {
@@ -18,4 +21,22 @@ const skills = [
   function getOne(id) {
     // Use the Array.prototype.find iterator method
     return skills.find(skill => skill.id === parseInt(id));
+  }
+
+  function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skill.level = '';
+    skills.push(skill);
+  }
+
+  function deleteOne(id) {
+    const idx = skills.findIndex(skill => skill.id === parseInt(id));
+    console.log('this is th idx', idx)
+    skills.splice(idx, 1);
+  }
+
+  function update(id, skill) {
+    const skillObj = skills.find(t => s.id === parseInt(id));
+    Object.assign(skillObj, skill);
   }
