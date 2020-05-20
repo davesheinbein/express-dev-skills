743640e3b7d32df4dd3843a0549fb6ed03d1b78c
David Sheinbein <davidsheinbein@Davids-MacBook-Pro.local>
2020-05-19 18:33:36 -0700

    updated

--- models/skill.js
+++ models/skill.js
@@ -1,7 +1,9 @@
 const skills = [
-    {id: 100001, skill: 'Karate', done: true},
-    {id: 100002, skill: 'Massage', done: false},
-    {id: 100003, skill: 'Shopping', done: false}
+    {id: 100001, skill: 'Karate', done: false},
+    {id: 100002, skill: 'Massage', done: true},
+    {id: 100003, skill: 'Shopping', done: false},
+    {id: 100004, skill: 'Cooking', done: true},
+    {id: 100005, skill: 'Sleeping', done: true}
   ];
 
   module.exports = {
